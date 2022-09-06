1c86309541442eb326425a4244c47221652c93a8
Claudia Rogoz <claudia.rogoz93@gmail.com>
2022-09-06 09:05:01 +0200

    Update libmamba/include/mamba/core/problems_graph_util.hpp
    Co-authored-by: Antoine Prouvost <AntoinePrv@users.noreply.github.com>

--- libmamba/include/mamba/core/problems_graph_util.hpp
+++ libmamba/include/mamba/core/problems_graph_util.hpp
@@ -81,14 +81,6 @@ namespace mamba
         };
     }
 
-    template <class... Ts>
-    struct overload : Ts...
-    {
-        using Ts::operator()...;
-    };
-    template <class... Ts>
-    overload(Ts...) -> overload<Ts...>;
-
     class MNode
     {
     public:
